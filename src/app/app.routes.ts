3713a88fabb015fd4b9aa47d8306fa86b89628c5
thiagoclrss <thiagocolaress77@gmail.com>
2024-05-02 12:00:16 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { Component } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "login",
+        component: LoginComponent
+    }
+];
